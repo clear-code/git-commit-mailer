f848d3bea41fd71d61ead5f059d528aec58b176a
Kenji Okimoto <okimoto@clear-code.com>
2015-03-19 21:42:48 +0900

    Move GitCommitMailer::CommitInfo::TextMailBodyFormatter

--- lib/git-commit-mailer/commit-info.rb
+++ lib/git-commit-mailer/commit-info.rb
@@ -343,76 +343,9 @@ class GitCommitMailer
       end
     end
 
-    class TextMailBodyFormatter < MailBodyFormatter
-      def format
-        super.sub(/\n+\z/, "\n")
-      end
-
-      private
-      def template
-        <<-EOT
-<%= @info.author_name %>\t<%= @mailer.format_time(@info.date) %>
-
-
-  New Revision: <%= @info.revision %>
-<%= format_commit_url %>
-
-<% unless @info.merge_status.empty? %>
-<%   @info.merge_status.each do |status| %>
-  <%= status %>
-<%   end %>
-
-<% end %>
-  Message:
-<% @info.summary.rstrip.each_line do |line| %>
-    <%= line.rstrip %>
-<% end %>
-
-<%= format_files("Added",        @info.added_files) %>
-<%= format_files("Copied",       @info.copied_files) %>
-<%= format_files("Removed",      @info.deleted_files) %>
-<%= format_files("Modified",     @info.updated_files) %>
-<%= format_files("Renamed",      @info.renamed_files) %>
-<%= format_files("Type Changed", @info.type_changed_files) %>
-
-<%= format_diff %>
-EOT
-      end
-
-      def format_commit_url
-        url = commit_url
-        return "" if url.nil?
-        "  #{url}\n"
-      end
-
-      def format_files(title, items)
-        return "" if items.empty?
-
-        formatted_files = "  #{title} files:\n"
-        items.each do |item_name, new_item_name|
-          if new_item_name.nil?
-            formatted_files << "    #{item_name}\n"
-          else
-            formatted_files << "    #{new_item_name}\n"
-            formatted_files << "      (from #{item_name})\n"
-          end
-        end
-        formatted_files
-      end
-
-      def format_diff
-        format_diffs.join("\n")
-      end
-
-      def format_diffs
-        @info.diffs.collect do |diff|
-          diff.format
-        end
-      end
-    end
-
   end
 end
 
 require "git-commit-mailer/commit-info/file-diff"
+require "git-commit-mailer/commit-info/text-mail-body-formatter"
 require "git-commit-mailer/commit-info/html-mail-body-formatter"
